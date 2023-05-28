1c07cc398c3a842546cbe74e9b5e9c125ea4daa0
Evgesha <Slav.novikova2014@gmail.com>
2023-05-28 22:43:53 +0700

    added Post

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Post {
+public class Post {
+    
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public String subscription; // поле для получения смс
+    public FormDate birthday;
 }
