a057203b1332aa4414985afdc03567d16fa98952
Evgesha <Slav.novikova2014@gmail.com>
2023-05-31 16:29:21 +0700

    change Post

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,10 +1,11 @@
 public class Post {
-    
+
     public String name; // поле для имени
     public String passport; // поле для паспорта
     public String patronymic; // поле для для отчества
     public String phone; // поле для телефона
     public String surname; // поле для фамилии
-    public String subscription; // поле для получения смс
+    public boolean subscription; // поле для получения смс
     public FormDate birthday;
+    
 }
